6926ad4b0686bfe0f6427984f0221fefba39c226
Nazar Buk <nazarbuk27@gmail.com>
2025-01-28 20:45:46 +0200

    Add admin auth.

---
+++ middleware/adminAuth.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+
+const adminAuth = async (req, res, next) => {
+  try {
+    const { token } = req.headers;
+
+    if (!token) {
+      return res.json({
+        success: false,
+        message: "Not Authorized, Login Again!",
+      });
+    }
+
+    const token_decode = jwt.verify(token, process.env.JWT_SECRET); // розкодовую токен
+
+    if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+      return res.json({
+        success: false,
+        message: "Not Authorized, Login Again!",
+      });
+    }
+
+    next(); // Виклик next() означає: "Ця middleware завершила свою роботу, передай управління наступному коду в стеку обробки".
+    // Якщо next() не викликати, обробка запиту "зависне", і сервер не відповість клієнту.
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export default adminAuth;
