ad0753d7b0b93ea270848f1d953eea3a6adbdabe
Nazar Buk <nazarbuk27@gmail.com>
2025-04-17 20:47:30 +0300

    🏗️ Create get and remove category endpoints with routes

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,9 +1,17 @@
 import express from "express";
 
-import { addCategory } from "../controllers/categoryController.js";
+import {
+  addCategory,
+  getCategories,
+  removeCategory,
+} from "../controllers/categoryController.js";
+
+import adminAuth from "../middleware/adminAuth.js";
 
 const categoryRouter = express.Router();
 
 categoryRouter.post("/add", addCategory);
+categoryRouter.get("/list", getCategories);
+categoryRouter.delete("/remove/:categoryId", adminAuth, removeCategory);
 
 export default categoryRouter;
