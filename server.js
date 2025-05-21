3aa2056f421008df6e981f8d1c623529255ab17a
Nazar Buk <nazarbuk27@gmail.com>
2025-05-21 17:42:43 +0300

    🏗️ Registaration/Logination

--- server.js
+++ server.js
@@ -8,12 +8,17 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import cookieParser from "cookie-parser"; // Для роботи з cookie
+import helmet from "helmet"; // Захист від базових атак
+import rateLimit from "express-rate-limit"; // Захист від спаму
 
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
 import categoryRouter from "./routes/categoryRoute.js";
+
+const allowOrigins = ["http://localhost:5173", "http://localhost:5174"];
 
 // App config
 const app = express();
@@ -23,7 +28,37 @@ connectCloudinary(); // підключив cloudinary, код до неї є в 
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+// app.use(cors()); //  так було у відео
+// app.use(cors({ origin: "http://localhost:4000", credentials: true })); // Дозволяємо запити з фронтенду
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Якщо запит не має origin (наприклад, з Postman), дозволити
+      if (!origin) return callback(null, true);
+      // origin: function (...) — дозволяє вручну перевіряти, чи походить запит з дозволеного джерела.
+
+      // Увага!!!
+      // credentials: 'include', // без цього куки не підуть  це треба додати на фронт,
+      // СТАВ ЦЕ ТОДІ КОЛИ :
+      //  Запити, що потребують аутентифікації / авторизації
+      // Наприклад, login, logout, profile, orders, user/settings — будь-які, де сервер очікує куки з токеном.
+
+      // Запити, де сервер повертає куки
+      // Наприклад, реєстрація користувача, логін, або оновлення сесії.
+
+      if (allowOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // credentials: true — потрібен, якщо ти використовуєш res.cookie(...) і хочеш, щоб куки передавалися між фронтом і бекендом.
+  })
+);
+
+app.use(helmet()); // Безпека
+app.use(cookieParser()); // Читання cookie
+app.use(rateLimit({ windowMs: 15 * 60 * 100, max: 100 })); // Обмеження 100 запитів за 15 хв
 
 //API Endpoints
 app.use("/api/user", userRouter); // /api/user/register,   /api/user/login,  /api/user/admin
