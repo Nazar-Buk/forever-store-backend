8721ae08e6d0079bea1cb9cf108f0a049338fe5a
Nazar Buk <nazarbuk27@gmail.com>
2025-10-27 02:04:11 +0200

    Add endpoints for forgot-password, reset-password, send reset message

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -9,6 +9,7 @@ import {
   removeUser,
   updateUserData,
   forgotPassword,
+  resetPassword,
 } from "../controllers/userController.js";
 import adminAuth from "../middleware/adminAuth.js";
 import verifyUser from "../middleware/userAuth.js";
@@ -25,5 +26,6 @@ userRouter.patch("/:id/role", adminAuth(["super-admin"]), updateUserRole);
 userRouter.delete("/:id", adminAuth(["super-admin"]), removeUser);
 userRouter.put("/update", verifyUser, updateUserData);
 userRouter.post("/forgot-password", forgotPassword);
+userRouter.post("/reset-password", resetPassword);
 
 export default userRouter;
